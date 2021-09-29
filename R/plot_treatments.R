a07434204ea065b57cbf2f31b041ee2a136a6932
Renata Diaz <diaz.renatam@gmail.com>
2021-09-29 16:42:23 -0400

    docs for plot_treatments

---
+++ R/plot_treatments.R
@@ -0,0 +1,6 @@
+#' Table of plot treatments over switches
+#'
+#' Reconstructed from Portal documentation (https://github.com/weecology/PortalData) and code supporting Christensen et al. 2019 (https://github.com/emchristensen/PlotSwitch/releases/tag/v2.0.0)
+#'
+#' @format A dataframe
+"plot_treatments"
